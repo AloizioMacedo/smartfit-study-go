8726ab00f8115ab8ceece6e877876dedf160fd78
AloizioMacedo <atsam2@hotmail.com>
2023-10-11 00:22:27 -0300

    Add endpoint to results

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Results struct {
+	Results []Result
+}
+
 type Result struct {
 	OpenClass    string
 	OpenedStatus string
@@ -25,6 +29,34 @@ func index(c *gin.Context) {
 	c.HTML(200, "index.html", nil)
 }
 
+func results(c *gin.Context) {
+	// day_period := c.Query("day_period")
+	// show_closed := c.DefaultQuery("show_closed", "false")
+
+	c.HTML(200, "results.html", Results{
+		Results: []Result{
+			{
+				OpenClass:    "closed",
+				OpenedStatus: "Closed",
+				Title:        "Closed",
+				Address:      "Closed",
+				Prohibs: []Prohib{
+					{
+						ProhibSource: "Closed",
+						Alt:          "Closed",
+					},
+				},
+				Schedules: []Schedule{
+					{
+						Weekdays: "Closed",
+						Hour:     "Closed",
+					},
+				},
+			},
+		},
+	})
+}
+
 func main() {
 	r := gin.Default()
 	r.StaticFile("style.css", "./templates/style.css")
@@ -32,6 +64,7 @@ func main() {
 
 	r.LoadHTMLGlob("templates/*.html")
 	r.GET("/", index)
+	r.GET("/results", results)
 
 	r.Run()
 }
