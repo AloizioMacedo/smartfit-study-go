c89808e31da07fb59df23fee4125994e566a337b
AloizioMacedo <atsam2@hotmail.com>
2023-10-11 20:06:55 -0300

    Fix values to provide to endpoint results

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"smartfit/locations"
 
@@ -39,12 +40,28 @@ func clean(c *gin.Context) {
 
 }
 
+func get_source(requirement string, obj string) string {
+	var req string
+	if requirement == "allowed" {
+		req = "required"
+	} else if requirement == "not_allowed" || requirement == "closed" {
+		req = "forbidden"
+	} else {
+		req = requirement
+	}
+
+	return fmt.Sprintf("images/%s-%s.png", req, obj)
+}
+
+func get_alt(requirement string, obj string) string {
+	return fmt.Sprintf("%s-%s", requirement, obj)
+}
+
 func parse_locations(locations []locations.Location) Results {
 	var results []Result
 
 	for _, location := range locations {
 		var schedules []Schedule
-
 		for _, schedule := range location.Schedules {
 			schedules = append(schedules, Schedule{
 				Weekdays: schedule.Weekdays,
@@ -52,15 +69,36 @@ func parse_locations(locations []locations.Location) Results {
 			})
 		}
 
+		var open_class string
+		var opened_status string
+		if location.Opened {
+			open_class = "open-facility"
+			opened_status = "Aberto"
+		} else {
+			open_class = "closed-facility"
+			opened_status = "Fechado"
+		}
+
 		results = append(results, Result{
-			OpenClass:    "Closed",
-			OpenedStatus: "Closed",
+			OpenClass:    open_class,
+			OpenedStatus: opened_status,
 			Title:        location.Title,
 			Address:      template.HTML(location.Content),
 			Prohibs: []Prohib{
 				{
-					ProhibSource: "Closed",
-					Alt:          "Closed",
+					ProhibSource: get_source(location.Mask, "mask"),
+					Alt:          get_alt(location.Mask, "mask"),
+				}, {
+					ProhibSource: get_source(location.Towel, "towel"),
+					Alt:          get_alt(location.Towel, "towel"),
+				},
+				{
+					ProhibSource: get_source(location.Fountain, "fountain"),
+					Alt:          get_alt(location.Fountain, "fountain"),
+				},
+				{
+					ProhibSource: get_source(location.Locker_room, "lockerroom"),
+					Alt:          get_alt(location.Locker_room, "lockerroom"),
 				},
 			},
 			Schedules: schedules,
